abc1e3c995aff0b08b2ea6e9bbdff5cdbc09d2bc
idk-GIT-alr <idk384005@gmail.com>
2026-05-18 11:40:34 +0530

    Time: 539 ms (5.03%) | Memory: 20.8 MB (23.88%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // int ans =0;
+
+        // for(int val: nums){
+        //     ans ^= val;
+        // }
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            int count =0;
+            for(int j=0; j<nums.size(); j++)
+            {
+                if(nums[i] == nums[j])
+                {
+                    count ++;
+                }
+            }
+            if(count == 1)
+            {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+};
